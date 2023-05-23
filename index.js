0c89062f0f8ed6aaa0ff1c4fc3c3adcdbe933b1b
devel12 <gzlakshya@gmail.com>
2023-05-23 22:13:00 +0530

    recursion upto 3 tries

--- index.js
+++ index.js
@@ -233,4 +233,8 @@ app.post('/reviews', async (req, res) => {
   }
 });
 
- 
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}.`);
+});
